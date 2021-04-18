a3df253592cf9f1ff0ff45fef916f5331feaeb32
Dayo Adediran <gistwithdayo@yahoo.com>
2021-04-17 20:32:48 -0400

    Wired test data to Items List box

--- ConsignmentAppUI/ConsignmentShop.cs
+++ ConsignmentAppUI/ConsignmentShop.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsignmentShopLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,55 @@ namespace ConsignmentAppUI
 {
     public partial class ConsignmentShop : Form
     {
+        private Store store = new Store();
+        BindingSource itemBinding = new BindingSource();
         public ConsignmentShop()
         {
             InitializeComponent();
+            SetupData();
+
+            itemBinding.DataSource = store.Items;
+            itemsListBox.DataSource = itemBinding;
+
+            itemsListBox.DisplayMember = "Display";
+            itemsListBox.ValueMember = "Display";
+
+        }
+        private void SetupData()
+        {
+            store.Vendors.Add(new Vendor { FirstName = "David", LastName = "Ayo" });
+            store.Vendors.Add(new Vendor { FirstName = "Joseph", LastName = "Tope" });
+
+            store.Items.Add(new Item
+            {
+                Title = "Dele Momo",
+                Description = "A book about ovation",
+                Price = 4.50M,
+                Owner = store.Vendors[0]
+            });
+            store.Items.Add(new Item
+            {
+                Title = "Cynthia Morgan",
+                Description = "A book about German Juice",
+                Price = 3.80M,
+                Owner = store.Vendors[1]
+            });
+            store.Items.Add(new Item
+            {
+                Title = "Micheal Cena",
+                Description = "A book about wresting",
+                Price = 5.20M,
+                Owner = store.Vendors[1]
+            });
+            store.Items.Add(new Item
+            {
+                Title = "Wasiu Ayinde",
+                Description = "A book about Fuji",
+                Price = 1.50M,
+                Owner = store.Vendors[0]
+            });
+
+            store.Name = "Na name of store be this";
         }
     }
 }
